71f8db56dae4a48d8e364e6bf548e326bb7aba1f
irmakapetanovic <irmakapetanovic@ecf527d8-cff1-4717-cbce-97b29af84b3a>
2010-12-08 00:16:14 +0000

    pocetak ozbiljnijeg validiranja

--- DZ3/DZ3/UnosKorisnika.h
+++ DZ3/DZ3/UnosKorisnika.h
@@ -68,17 +68,29 @@ namespace DZ3 {
 	private: System::Windows::Forms::Label^  label2;
 	private: System::Windows::Forms::TabPage^  tabPage2;
 	private: System::Windows::Forms::Label^  label9;
-	private: System::Windows::Forms::TextBox^  c_PDV_broj;
+
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::TextBox^  c_naziv_firme;
 	private: System::Windows::Forms::StatusStrip^  statusStrip1;
+
+	private: System::Windows::Forms::ToolStripStatusLabel^  toolStripStatusLabel1;
+	private: System::Windows::Forms::ErrorProvider^  errorProvider1;
+
+	private: System::Windows::Forms::MaskedTextBox^  c_PDV_broj;
 	private: KontrolaUnos::KontrolaUnosKorisnika^  kontrolaUnosKorisnika1;
+
+
+
+	private: System::ComponentModel::IContainer^  components;
+
+
+
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -87,6 +99,7 @@ namespace DZ3 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->Unesi = (gcnew System::Windows::Forms::Button());
 			this->tabControl2 = (gcnew System::Windows::Forms::TabControl());
@@ -98,15 +111,19 @@ namespace DZ3 {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->c_PDV_broj = (gcnew System::Windows::Forms::MaskedTextBox());
 			this->label9 = (gcnew System::Windows::Forms::Label());
-			this->c_PDV_broj = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->c_naziv_firme = (gcnew System::Windows::Forms::TextBox());
 			this->statusStrip1 = (gcnew System::Windows::Forms::StatusStrip());
+			this->toolStripStatusLabel1 = (gcnew System::Windows::Forms::ToolStripStatusLabel());
+			this->errorProvider1 = (gcnew System::Windows::Forms::ErrorProvider(this->components));
 			this->kontrolaUnosKorisnika1 = (gcnew KontrolaUnos::KontrolaUnosKorisnika());
 			this->tabControl2->SuspendLayout();
 			this->tabPage1->SuspendLayout();
 			this->tabPage2->SuspendLayout();
+			this->statusStrip1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->errorProvider1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button5
@@ -118,6 +135,7 @@ namespace DZ3 {
 			this->button5->TabIndex = 1;
 			this->button5->Text = L"Izlaz";
 			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &UnosKorisnika::button5_Click);
 			// 
 			// Unesi
 			// 
@@ -128,6 +146,7 @@ namespace DZ3 {
 			this->Unesi->TabIndex = 0;
 			this->Unesi->Text = L"Unos";
 			this->Unesi->UseVisualStyleBackColor = true;
+			this->Unesi->Click += gcnew System::EventHandler(this, &UnosKorisnika::Unesi_Click);
 			// 
 			// tabControl2
 			// 
@@ -164,6 +183,7 @@ namespace DZ3 {
 			this->c_broj_licne_karte->Name = L"c_broj_licne_karte";
 			this->c_broj_licne_karte->Size = System::Drawing::Size(172, 20);
 			this->c_broj_licne_karte->TabIndex = 3;
+			this->c_broj_licne_karte->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &UnosKorisnika::c_broj_licne_karte_Validating);
 			// 
 			// label8
 			// 
@@ -180,6 +200,7 @@ namespace DZ3 {
 			this->c_prezime->Name = L"c_prezime";
 			this->c_prezime->Size = System::Drawing::Size(172, 20);
 			this->c_prezime->TabIndex = 2;
+			this->c_prezime->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &UnosKorisnika::c_prezime_Validating);
 			// 
 			// c_ime
 			// 
@@ -187,6 +208,7 @@ namespace DZ3 {
 			this->c_ime->Name = L"c_ime";
 			this->c_ime->Size = System::Drawing::Size(172, 20);
 			this->c_ime->TabIndex = 1;
+			this->c_ime->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &UnosKorisnika::c_ime_Validating);
 			// 
 			// label3
 			// 
@@ -210,8 +232,8 @@ namespace DZ3 {
 			// 
 			this->tabPage2->BackColor = System::Drawing::Color::White;
 			this->tabPage2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
-			this->tabPage2->Controls->Add(this->label9);
 			this->tabPage2->Controls->Add(this->c_PDV_broj);
+			this->tabPage2->Controls->Add(this->label9);
 			this->tabPage2->Controls->Add(this->label1);
 			this->tabPage2->Controls->Add(this->c_naziv_firme);
 			this->tabPage2->Location = System::Drawing::Point(4, 22);
@@ -221,6 +243,16 @@ namespace DZ3 {
 			this->tabPage2->TabIndex = 1;
 			this->tabPage2->Text = L"Firma";
 			// 
+			// c_PDV_broj
+			// 
+			this->c_PDV_broj->Location = System::Drawing::Point(107, 60);
+			this->c_PDV_broj->Mask = L"000000000000";
+			this->c_PDV_broj->Name = L"c_PDV_broj";
+			this->c_PDV_broj->Size = System::Drawing::Size(80, 20);
+			this->c_PDV_broj->TabIndex = 11;
+			this->c_PDV_broj->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->c_PDV_broj->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &UnosKorisnika::c_PDV_broj_Validating);
+			// 
 			// label9
 			// 
 			this->label9->AutoSize = true;
@@ -229,13 +261,6 @@ namespace DZ3 {
 			this->label9->Size = System::Drawing::Size(52, 13);
 			this->label9->TabIndex = 10;
 			this->label9->Text = L"PDV broj:";
-			// 
-			// c_PDV_broj
-			// 
-			this->c_PDV_broj->Location = System::Drawing::Point(107, 53);
-			this->c_PDV_broj->Name = L"c_PDV_broj";
-			this->c_PDV_broj->Size = System::Drawing::Size(172, 20);
-			this->c_PDV_broj->TabIndex = 1;
 			// 
 			// label1
 			// 
@@ -252,14 +277,26 @@ namespace DZ3 {
 			this->c_naziv_firme->Name = L"c_naziv_firme";
 			this->c_naziv_firme->Size = System::Drawing::Size(172, 20);
 			this->c_naziv_firme->TabIndex = 0;
+			this->c_naziv_firme->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &UnosKorisnika::c_naziv_firme_Validating);
 			// 
 			// statusStrip1
 			// 
+			this->statusStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->toolStripStatusLabel1});
 			this->statusStrip1->Location = System::Drawing::Point(0, 455);
 			this->statusStrip1->Name = L"statusStrip1";
 			this->statusStrip1->Size = System::Drawing::Size(342, 22);
 			this->statusStrip1->TabIndex = 100;
 			this->statusStrip1->Text = L"statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this->toolStripStatusLabel1->BackColor = System::Drawing::SystemColors::Control;
+			this->toolStripStatusLabel1->Name = L"toolStripStatusLabel1";
+			this->toolStripStatusLabel1->Size = System::Drawing::Size(0, 17);
+			// 
+			// errorProvider1
+			// 
+			this->errorProvider1->ContainerControl = this;
 			// 
 			// kontrolaUnosKorisnika1
 			// 
@@ -273,6 +310,7 @@ namespace DZ3 {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoValidate = System::Windows::Forms::AutoValidate::EnablePreventFocusChange;
 			this->BackColor = System::Drawing::Color::White;
 			this->ClientSize = System::Drawing::Size(342, 477);
 			this->Controls->Add(this->kontrolaUnosKorisnika1);
@@ -283,11 +321,19 @@ namespace DZ3 {
 			this->MaximizeBox = false;
 			this->Name = L"UnosKorisnika";
 			this->Text = L"Unos novog korisnika";
+			this->CursorChanged += gcnew System::EventHandler(this, &UnosKorisnika::UnosKorisnika_CursorChanged);
+			this->Shown += gcnew System::EventHandler(this, &UnosKorisnika::UnosKorisnika_Shown);
+			this->AutoValidateChanged += gcnew System::EventHandler(this, &UnosKorisnika::UnosKorisnika_AutoValidateChanged);
+			this->Activated += gcnew System::EventHandler(this, &UnosKorisnika::UnosKorisnika_Activated);
+			this->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &UnosKorisnika::UnosKorisnika_MouseMove);
 			this->tabControl2->ResumeLayout(false);
 			this->tabPage1->ResumeLayout(false);
 			this->tabPage1->PerformLayout();
 			this->tabPage2->ResumeLayout(false);
 			this->tabPage2->PerformLayout();
+			this->statusStrip1->ResumeLayout(false);
+			this->statusStrip1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->errorProvider1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -295,5 +341,173 @@ namespace DZ3 {
 #pragma endregion
 	private: System::Void groupBox1_Enter(System::Object^  sender, System::EventArgs^  e) {
 			 }
+private: System::Void kontrolaUnosKorisnika1_Enter(System::Object^  sender, System::EventArgs^  e) {
+
+		 }
+private: System::Void kontrolaUnosKorisnika1_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+			
+		 }
+private: System::Void kontrolaUnosKorisnika1_MouseEnter(System::Object^  sender, System::EventArgs^  e) {
+			
+		 }
+private: System::Void kontrolaUnosKorisnika1_MouseMove(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+
+		 }
+private: System::Void UnosKorisnika_Activated(System::Object^  sender, System::EventArgs^  e) {
+			
+		 }
+private: System::Void UnosKorisnika_MouseMove(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+			
+		 }
+private: System::Void UnosKorisnika_Shown(System::Object^  sender, System::EventArgs^  e) {
+			
+		 }
+private: System::Void UnosKorisnika_CursorChanged(System::Object^  sender, System::EventArgs^  e) {
+			
+		 }
+private: System::Void UnosKorisnika_AutoValidateChanged(System::Object^  sender, System::EventArgs^  e) {
+			
+		 }
+private: System::Void c_ime_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+			 if (c_ime->Text->Length < 3)
+			 {
+				 c_ime->Focus ();
+				 toolStripStatusLabel1->ForeColor = Color::Red;
+				 toolStripStatusLabel1->Text = "Ime ne smije sadržavati manje od 3 slova.";
+				 errorProvider1->SetError (c_ime, "Ime ne smije sadržavati manje od 3 slova.");				
+			 }
+			 else
+			 {
+				errorProvider1->Clear ();
+				toolStripStatusLabel1->Text = "";
+			 }
+
+						 
+		 }
+private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
+			 Close ();
+		 }
+private: System::Void Unesi_Click(System::Object^  sender, System::EventArgs^  e) {
+
+			 try
+			 {
+				 kontrolaUnosKorisnika1->setAdresa ();
+				 kontrolaUnosKorisnika1->setTelefon ();
+				 //kontrolaUnosKorisnika1->setPaket ();
+				 kontrolaUnosKorisnika1->setUsername ();
+				 kontrolaUnosKorisnika1->setPassword ();
+				 kontrolaUnosKorisnika1->setModem ();
+			 }
+			 catch (...)
+			 {
+				 toolStripStatusLabel1->ForeColor = Color::Red;
+				 toolStripStatusLabel1->Text = "Podaci nisu spašeni zbog pogrešnog unosa.";
+				 MessageBox::Show ("Podaci nisu spašeni zbog pogrešnog unosa.", "Greška pri unosu", MessageBoxButtons::OK, MessageBoxIcon::Warning) ;				 
+			 }
+	
+
+
+		 }
+private: System::Void c_prezime_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+			if (c_prezime->Text->Length < 3)
+			 {
+				 c_prezime->Focus ();
+				 toolStripStatusLabel1->ForeColor = Color::Red;
+				 toolStripStatusLabel1->Text = "Prezime ne smije sadržavati manje od 3 slova.";
+				 errorProvider1->SetError (c_prezime, "Prezime ne smije sadržavati manje od 3 slova.");				
+			 }
+			else
+			 {
+				errorProvider1->Clear ();
+				toolStripStatusLabel1->Text = "";
+			 }
+
+		 }
+private: System::Void c_broj_licne_karte_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+			 if (c_broj_licne_karte->Text->Length < 9)
+			 {
+				 c_broj_licne_karte->Focus ();
+				 toolStripStatusLabel1->ForeColor = Color::Red;
+				 toolStripStatusLabel1->Text = "Broj liène karte ne smije sadržavati manje od 9 znakova.";
+				 errorProvider1->SetError (c_broj_licne_karte, "Prezime ne smije sadržavati manje od 9 znakova.");	
+			 }
+			else if (c_broj_licne_karte->Text->Length > 9)
+			 {
+				 c_broj_licne_karte->Focus ();
+				 toolStripStatusLabel1->ForeColor = Color::Red;
+				 toolStripStatusLabel1->Text = "Broj liène karte ne smije sadržavati više od 9 znakova.";
+				 errorProvider1->SetError (c_broj_licne_karte, "Prezime ne smije sadržavati više od 9 znakova.");	
+			 }
+			else if (c_broj_licne_karte->Text->Length == 9)
+			 {
+				 wchar_t prvo = c_broj_licne_karte->Text [0];
+				 wchar_t drugo = c_broj_licne_karte->Text [1];
+				 wchar_t sesto = c_broj_licne_karte->Text [5];
+				 wchar_t sedmo = c_broj_licne_karte->Text [6];
+				 wchar_t osmo = c_broj_licne_karte->Text [7];
+				 wchar_t deveto = c_broj_licne_karte->Text [8];			
+				 /*
+				 int trece = c_broj_licne_karte->Text [2];
+				 int cetvrto = c_broj_licne_karte->Text [3];
+				 int peto = c_broj_licne_karte->Text [4];
+				 */
+
+
+				 if ((prvo < '0' || drugo < '0' || sesto < '0' || sedmo < '0' || osmo < '0' || deveto < '0')  || (prvo > '9' || drugo > '9' || sesto > '9' || sedmo > '9' || osmo > '9' || deveto > '9') )
+				 {
+					c_broj_licne_karte->Focus ();
+					toolStripStatusLabel1->ForeColor = Color::Red;
+					toolStripStatusLabel1->Text = "Format broja liène karte treba biti: 2 slova + 3 slova + 4 broja.";
+					errorProvider1->SetError (c_broj_licne_karte, "Format broja liène karte treba biti: 2 slova + 3 slova + 4 broja.");
+				 }
+				/*
+				 if ((trece || cetvrto || peto) < 'A' || (trece || cetvrto || peto) > 'Z')
+				 {
+					c_broj_licne_karte->Focus ();
+					toolStripStatusLabel1->ForeColor = Color::Red;
+					toolStripStatusLabel1->Text = "Treci, cetvrti i peti znak moraju biti slova.";
+					errorProvider1->SetError (c_broj_licne_karte, "Treci, cetvrti i peti znak moraju biti slova.");
+				 }
+				 */
+				 else // ovaj mi nesto ne radi
+				 {
+					errorProvider1->Clear ();
+					toolStripStatusLabel1->Text = "";
+				 }
+
+			 }
+			 			 
+
+		 }
+private: System::Void c_PDV_broj_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+			 if (c_PDV_broj->Text->Length < 12)
+			 {
+				c_PDV_broj->Focus ();
+				toolStripStatusLabel1->ForeColor = Color::Red;
+				toolStripStatusLabel1->Text = "PDV broj mora imati 12 cifara.";
+				errorProvider1->SetError (c_PDV_broj, "PDV broj mora imati 12 cifara.");
+			 }
+			 else 
+			 {
+				errorProvider1->Clear ();
+				toolStripStatusLabel1->Text = "";
+			 }
+		 }
+private: System::Void c_naziv_firme_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+			 if (c_naziv_firme->Text->Length < 3)
+			 {
+				toolStripStatusLabel1->ForeColor = Color::Red;
+				toolStripStatusLabel1->Text = "Naziv firme ne može imati manje od tri slova.";
+				errorProvider1->SetError (c_naziv_firme, "Naziv firme ne može imati manje od tri slova.");
+				c_naziv_firme->Focus ();
+			 }
+			 
+			//ovdje se mora ispitati da ne bude brojeva u nazivu
+			 else 
+			 {
+				errorProvider1->Clear ();
+				toolStripStatusLabel1->Text = "";
+			 }
+		 }
 };
 }
